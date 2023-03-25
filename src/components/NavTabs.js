d297992f511e4caa7a10b7482f974c46c0384b14
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-03-25 09:51:15 -0600

    add about me content

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,45 +1,54 @@
-import React from 'react';
+import React from "react";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className="nav justify-content-between">
+      <h2>Josh Augsburger</h2>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+            }
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link active" : "nav-link"
+            }
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <h1>About Me</h1>
+      <div>
+        <img src="images/about-pic.jpeg" alt="Josh Augsburger Picture"></img>
+        <p>
+          I am the Engineering Manager for Smart Bar USA. Smart bar USA is a
+          company that provides automated cocktail dispensers to the Hospitality
+          industry. I oversee all engineering activities and development
+          personnel for the business. Currently this includes a dedicated
+          contract software development team of 3 developers, and one contract
+          electronics engineer and mechanical engineer who work on an as needed
+          basis for Smart Bar USA. In addition to managing the engineering team,
+          I also do most of the mechanical design, R&D, testing, and validation
+          of all of Smart Bar USA's products.<br></br>
+          <br></br>
+          As the son of the local small engine repair shop owner located in a
+          small farming community in Ohio, it was an easy decision to get a
+          Bachelors Degree in Mechanical Engineering after high school. After
+          college I worked with GE Appliances in Louisvillle, KY. While
+          completing the GE rotational engineering development program I got a
+          Masters Degree in Mechanical Engineering, specializing in appliance
+          design. I then decided to take a Senior Mechanical Engineering role
+          with Smart Bar USA and moved into the Engineering Manager posiition
+          for the company as the team grew. After a few years of managing the
+          team, I decided to try to learn what my software team was talking
+          about and signed up a Software Development Bootcamp. Since being in
+          the UC Berkeley Bootcamp, I have found software development to be very
+          interesting and fun!
+        </p>
+      </div>
     </div>
   );
 }
