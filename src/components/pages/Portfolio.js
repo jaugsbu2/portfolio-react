d1e792cb00fa291a6aea1c711d414080065d1cf7
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-03-26 10:01:02 -0600

    finish portfolio content

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,21 +1,92 @@
-import React from 'react';
+import React from "react";
+import "../../styles/portfolio.css";
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="work">
+      <h1>My work</h1>
+      <div className="workcontainer">
+        <div className="napster">
+          <a
+            href="https://jaugsbu2.github.io/napster2.0-with-youtube/"
+            target={"_blank"}
+            className="button"
+          ></a>
+          <div className="cardText">
+            <a
+              href="https://jaugsbu2.github.io/napster2.0-with-youtube/"
+              target={"_blank"}
+            >
+              Napster Music Video Play Website
+            </a>
+            <a
+              href="https://github.com/jaugsbu2/napster2.0-with-youtube"
+              target={"_blank"} className="githubLogo"
+            >
+            </a>
+          </div>
+        </div>
+        <div className="herd">
+          <a
+            href="https://cattle-tracker.herokuapp.com/"
+            target={"_blank"}
+            className="button"
+          ></a>
+          <div className="cardText">
+            <a
+              href="https://cattle-tracker.herokuapp.com/"
+              target={"_blank"}
+            >
+              Herd Cattle Tracker
+            </a>
+            <a
+              href="https://github.com/malxxy/cattle-tracker"
+              target={"_blank"} className="githubLogo"
+            >
+            </a>
+          </div>
+        </div>
+        <div className="blog">
+          <a
+            href="https://mysterious-beyond-13173.herokuapp.com/"
+            target={"_blank"}
+            className="button"
+          ></a>
+          <div className="cardText">
+            <a
+              href="https://mysterious-beyond-13173.herokuapp.com/"
+              target={"_blank"}
+            >
+              Tech Blog
+            </a>
+            <a
+              href="https://github.com/jaugsbu2/simple-blog"
+              target={"_blank"} className="githubLogo"
+            >
+            </a>
+          </div>
+        </div>
+        <div className="weather">
+          <a
+            href="https://jaugsbu2.github.io/06-challenge-weather-dashboard/"
+            target={"_blank"}
+            className="button"
+          ></a>
+          <div className="cardText">
+            <a
+              href="https://jaugsbu2.github.io/06-challenge-weather-dashboard/"
+              target={"_blank"}
+            >
+              Weather Dashboard
+            </a>
+            <a
+              href="https://github.com/jaugsbu2/06-challenge-weather-dashboard"
+              target={"_blank"} className="githubLogo"
+            >
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
